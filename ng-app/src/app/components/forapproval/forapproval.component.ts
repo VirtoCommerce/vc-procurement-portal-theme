a81e436b4da0c238657397c65cb2b0ab6764f936
ilyawzrd <ilya.ivanov6@gmail.com>
2019-07-25 16:03:43 +0200

    Orders list for approval page

--- ng-app/src/app/components/forapproval/forapproval.component.ts
+++ ng-app/src/app/components/forapproval/forapproval.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationInfo } from 'src/app/models/inner/pagination-info';
+import { AppConfig } from 'src/app/services/app-config.service';
+import { OrdersService } from 'src/app/services/orders.service';
+import { PageSizeChangedArgs } from '../page-size-selector/page-size-selector.component';
+import { IOrder } from 'src/app/models/iorder';
 
 @Component({
   selector: 'app-forapproval',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forapproval.component.scss']
 })
 export class ForApprovalComponent implements OnInit {
+  paginationInfo = new PaginationInfo(AppConfig.settings.defaultPageSize);
+  pageSizes = AppConfig.settings.pageSizes;
+  orders: IOrder[] = [];
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) { }
 
   ngOnInit() {
+    console.log("forapproval component. getOrders");
+    this.getAllOrders();
+  }
+
+  pageSizeChanged(eventArgs: PageSizeChangedArgs) {
+    this.paginationInfo.pageSize = eventArgs.newPageSize;
+    this.getAllOrders();
+  }
+
+  getAllOrders() {
+    this.ordersService
+      .getOrders(this.paginationInfo.page, this.paginationInfo.pageSize)
+      .subscribe((data: any) => {
+        this.orders = data.results as IOrder[];
+        this.paginationInfo.collectionSize = data.totalCount;
+        console.log('Orders ', this.orders);
+      });
+  }
+
+  pageChanged() {
+    this.getAllOrders();
   }
 
 }
