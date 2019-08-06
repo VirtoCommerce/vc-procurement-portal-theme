2c9ff02bb6ff0b1b47d8903b1573d288c82d27f5
ilyawzrd <ilya.ivanov6@gmail.com>
2019-08-06 21:01:28 +0200

    Added user management features

--- ng-app/src/app/components/company/company.component.ts
+++ ng-app/src/app/components/company/company.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { OrganizationService } from "src/app/services/organization.service";
 import { IOrganization } from "src/app/models/dto/iorganization";
+import { IUser } from "src/app/models/dto/iuser";
+import { UserService } from "src/app/services/user.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import settings_data from "src/assets/config/config.dev.json";
+import { IAppConfig } from "src/app/models/iapp-config";
+import { PaginationInfo } from "src/app/models/inner/pagination-info";
+import { PageSizeChangedArgs } from "../page-size-selector/page-size-selector.component";
 
 @Component({
   selector: "app-company",
@@ -8,17 +15,114 @@ import { IOrganization } from "src/app/models/dto/iorganization";
   styleUrls: ["./company.component.scss"]
 })
 export class CompanyComponent implements OnInit {
+  users: IUser[];
   organization: IOrganization;
   companyName: string;
+  billingAddress: {
+    country: string;
+    city: string;
+    postalCode: string;
+    address1: string;
+    address2: string;
+  };
+  shippingAddress: {
+    country: string;
+    city: string;
+    postalCode: string;
+    address1: string;
+    address2: string;
+  };
 
+  settings = settings_data as IAppConfig;
+  paginationInfo = new PaginationInfo(this.settings.defaultPageSize);
+  pageSizes = this.settings.pageSizes;
 
-  constructor(private organizationService: OrganizationService) {}
+  constructor(
+    private organizationService: OrganizationService,
+    private userService: UserService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.organizationService.getUserOrganization().subscribe((data: any) => {
       this.organization = data as IOrganization;
       this.companyName = this.organization.name;
-    })
-    }
+      this.organization.addresses.forEach(address => {
+        if (address.type == 1) {
+          this.billingAddress = {
+            country: address.countryName,
+            city: address.city,
+            postalCode: address.postalCode,
+            address1: address.line1,
+            address2: address.line2
+          };
+        }
+        if (address.type == 2) {
+          this.shippingAddress = {
+            country: address.countryName,
+            city: address.city,
+            postalCode: address.postalCode,
+            address1: address.line1,
+            address2: address.line2
+          };
+        }
+      });
+    });
 
+    this.fetchUsers();
+  }
+
+  pageSizeChanged(eventArgs: PageSizeChangedArgs) {
+    this.paginationInfo.pageSize = eventArgs.newPageSize;
+    this.fetchUsers();
+  }
+
+  pageChanged() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.organizationService
+      .getOrganizationUsers(
+        this.paginationInfo.page,
+        this.paginationInfo.pageSize
+      )
+      .subscribe((data: any) => {
+        this.users = data.results;
+        this.paginationInfo.collectionSize = data.totalCount;
+        console.log("Users: ", this.users);
+      });
+  }
+
+  updateCompany() {
+    this.organizationService.updateOrganization(this.companyName).subscribe();
+  }
+
+  deleteUser(user: IUser) {
+    this.userService.deleteUser(user.id).subscribe(() => {
+      this.fetchUsers();
+    });
+  }
+
+  openCreateUserModal(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  addNewUser(form) {
+    const user = {
+      email: form.value.email,
+      firstName: form.value.firstName,
+      lastName: form.value.lastName,
+      organizationId: this.organization.id,
+      password: form.value.password,
+      role: form.value.selectRole,
+      //storeId:
+      userName: form.value.userName
+    };
+    //console.log(form.value)
+    this.userService.registerNewUser(user).subscribe(() => {
+      this.fetchUsers();
+      this.modalService.dismissAll();
+    });
+  }
 }
--- ng-app/src/app/services/organization.service.ts
+++ ng-app/src/app/services/organization.service.ts
@@ -1,41 +1,55 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { tap, catchError } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { tap, catchError } from "rxjs/operators";
+import { Observable, of } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrganizationService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUserOrganization() {
-    console.log('getUserOrganization');
-    return this.http.get('storefrontapi/account/organization/current?t=').pipe(
-        tap(
-            organization => {
-                this.log(`fetched organization:` + organization);
-            }),
-        catchError(this.handleError('organization', []))
+    console.log("getUserOrganization");
+    return this.http.get("storefrontapi/account/organization/current?t=").pipe(
+      tap(organization => {
+        this.log(`fetched organization:` + organization);
+      }),
+      catchError(this.handleError("organization", []))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-        // TODO: send the error to remote logging infrastructure
-        console.error(error); // log to console instead
+  getOrganizationUsers(pageNumber: number, pageSize: number) {
+    const url = 'storefrontapi/account/organization/users/search';
+    const body = { pageNumber: pageNumber, start: (pageNumber - 1) * pageSize, pageSize: pageSize};
+    return this.http.post<any>(url,body);
+  }
 
-        // TODO: better job of transforming error for user consumption
-        this.log(`${operation} failed: ${error.message}`);
-
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
+  updateOrganization(
+    newName: string
+  ) {
+    console.log('updating org with new name: ', newName)
+    const url = 'storefrontapi/account/organization';
+    const body = {
+      name: newName
     };
-}
+    return this.http.put<any>(url, body);
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
   private log(message: string) {
-    console.log('Organization service: ' + message);
-}
-
+    console.log("Organization service: " + message);
+  }
 }
