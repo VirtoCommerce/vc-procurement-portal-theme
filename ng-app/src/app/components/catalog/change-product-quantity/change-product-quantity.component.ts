1f6e7ba30dd96f429c4ecff57f957864a265f26e
Timofey <tburashov@gmail.com>
2019-09-27 09:55:40 +0700

    VP-429: rework the change-product-quantity.component
    additional validation for new quantity values
    
    the new quantity debouncing if the value of new quantity still in changing progress

--- ng-app/src/app/components/catalog/change-product-quantity/change-product-quantity.component.ts
+++ ng-app/src/app/components/catalog/change-product-quantity/change-product-quantity.component.ts
@@ -1,97 +1,131 @@
+import { AlertsService } from './../../../modules/alerts/alerts.service';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { ActiveOrderService } from '@api/active-order.service';
 import { ICart, ILineItem } from '@models/dto/icart';
 import { ConfirmService } from '@modules/confirm-modal/confirm-modal-service';
-import { Subject, Subscription, EMPTY } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-change-product-quantity',
   templateUrl: './change-product-quantity.component.html',
-  styleUrls: ['./change-product-quantity.component.scss']
+  styleUrls: ['./change-product-quantity.component.scss'],
 })
 export class ChangeProductQuantityComponent implements OnInit, OnDestroy {
-  @Input()
-  isProductPage = false;
-  @Input()
-  cart: ICart;
-  @Input()
-  productId: string;
+  private _blockQuantityChanging = false;
+  private _blockAddingToCart = false;
+  private quantityChangingSubscription$: Subscription;
+  private cartSubscription$: Subscription;
+  @Input() isProductPage = false;
+  @Input() cart: ICart;
+  @Input() productId: string;
+  public product: ILineItem;
+  public productQuantityChanging$ = new Subject<number>();
+  public quantity = 0;
 
-
-  productQuantity$ = new Subject<number>();
-  private quantitySub: Subscription;
-
-  constructor(private readonly activeOrderService: ActiveOrderService, private confirmService: ConfirmService) {
-   }
+  constructor(private readonly activeOrderService: ActiveOrderService,
+              private confirmService: ConfirmService,
+              private alertsService: AlertsService) {
+  }
 
   ngOnInit() {
-    this.quantitySub = this.productQuantity$.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      switchMap(quantity => {
-        console.log('New quantity: ', quantity);
-        this.updateLineItemQuantity(+quantity);
-        return EMPTY;
-      })
-    ).subscribe();
+    this.quantityChangingSubscription$ = this.productQuantityChanging$.pipe(
+      shareReplay(),
+      debounceTime(300),
+      distinctUntilChanged())
+      .subscribe(async newQuantity => {
+        if (!this._blockQuantityChanging) {
+          this.activeOrderService.changeItemQuantity(this.product.id, newQuantity).subscribe();
+        }
+      });
+
+    this.product = this.findProductInCart(this.cart, this.productId);
+    this.quantity = this.getProductQuantityInCart(this.product);
+
+    this.cartSubscription$ = this.activeOrderService.Cart.subscribe((cart: ICart) => {
+      this.product = this.findProductInCart(cart, this.productId);
+      this.quantity = this.getProductQuantityInCart(this.product);
+    });
   }
 
-  get productLineItem() {
-    return this.getProductLineItem(this.productId);
-  }
-
-  removeItem(item: ILineItem) {
-    const confirmOptions = { title: 'Line item removing', message: 'Are you sure you want to remove this line item from the active order?' };
-    this.confirmService.confirm(confirmOptions).then(() => this.activeOrderService.removeItem(item.id).subscribe(), () => { } );
-  }
-
-  private getProductLineItem(productId: string): ILineItem {
-     const lineItem = this.cart.items.find(x => x.productId === productId);
-     return lineItem;
-  }
-
-  addProductToCart() {
-    this.activeOrderService.addItem(this.productId).subscribe();
-  }
-
-
-  decrementQuantity(lineItem: ILineItem) {
-    if (lineItem.quantity <= 1) {
-      this.removeItem(lineItem);
-      return;
+  public addProductToCart() {
+    if (!this.isInCart(this.product) && !this._blockAddingToCart) {
+      this._blockAddingToCart = true;
+      this.activeOrderService.addItem(this.productId).subscribe(() => this._blockAddingToCart = false);
     }
-    lineItem.quantity--;
-    this.activeOrderService.changeItemQuantity(lineItem.id, lineItem.quantity).subscribe();
   }
 
-  incrementQuantity(lineItem: ILineItem) {
-    lineItem.quantity++;
-    this.activeOrderService.changeItemQuantity(lineItem.id, lineItem.quantity).subscribe();
-  }
+  public async changeQuantity(value: number, byStep: boolean) {
+    if (this.isInCart(this.product)) {
+      if (byStep) {
+        this.quantity += value;
+      } else {
+        this.quantity = +value;
+      }
+    }
 
-  onChangeQuantity(inputElement: HTMLInputElement) {
-    const quantity = +inputElement.value;
-    if (quantity) {
-      this.productQuantity$.next(quantity);
+    if (this.quantity < 1) {
+      if (await this.showApproveDeletionConfirmation(this.quantity)) {
+        this.activeOrderService.removeItem(this.product.id).subscribe();
+        return;
+      } else {
+        // rollback the initial product quantity
+        this.quantity = this.product.quantity;
+        return;
+      }
+    }
+
+    if (this.isMoreThanInStock(this.quantity)) {
+      this.alertsService.warn(`\"${this.product.name}\" is not in stock in the requested quantity ${this.quantity}. Available: ${this.product.inStockQuantity}`);
     } else {
-      inputElement.value = this.productLineItem.quantity.toString();
+      this.productQuantityChanging$.next(this.quantity);
     }
-  }
-
-  updateLineItemQuantity(quantity: number) {
-    const lineItem = this.productLineItem;
-
-    this.activeOrderService.changeItemQuantity(lineItem.id, quantity).subscribe();
   }
 
   ngOnDestroy(): void {
-    if (this.quantitySub) {
-      this.quantitySub.unsubscribe();
-      this.quantitySub = null;
+    if (this.quantityChangingSubscription$ != null) {
+      this.quantityChangingSubscription$.unsubscribe();
+      this.quantityChangingSubscription$ = null;
     }
 
+    if (this.cartSubscription$ != null) {
+      this.cartSubscription$.unsubscribe();
+      this.cartSubscription$ = null;
+    }
   }
 
+  private showApproveDeletionConfirmation(newQuantity: number): Promise<boolean> {
+    if (newQuantity < 1) {
+      const confirmOptions = {
+        title: 'Line item removing',
+        backdrop: 'static',
+        message: 'Are you sure you want to remove this line item from the active order?'
+      };
 
+      this._blockQuantityChanging = true;
+      return this.confirmService.confirm(confirmOptions).then(() => true, () => false).finally(() => {
+        this._blockQuantityChanging = false;
+      });
+    }
+  }
+
+  private isInCart(product: ILineItem): boolean {
+    return product != null ? true : false;
+  }
+
+  private isMoreThanInStock(newQuantity: number) {
+    return newQuantity > this.product.inStockQuantity;
+  }
+
+  private getProductQuantityInCart(product: ILineItem): number {
+    if (this.isInCart(product)) {
+      return this.product.quantity;
+    } else {
+      return 0;
+    }
+  }
+
+  private findProductInCart(cart: ICart, productId: string): ILineItem {
+    return this.product = cart.items.find(x => x.productId === productId);
+  }
 }
